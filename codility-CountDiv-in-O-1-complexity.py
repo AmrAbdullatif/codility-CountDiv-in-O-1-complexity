a5fd7e72f181cfd28c888558fa88b0635c9d7826
AmrAbdullatif <31695791+AmrAbdullatif@users.noreply.github.com>
2018-10-15 11:58:50 +0200

    Create codility-CountDiv-in-O-1-complexity.py

---
+++ codility-CountDiv-in-O-1-complexity.py
@@ -0,0 +1,13 @@
+def solution(A, B, K):
+    # write your code in Python 3.6
+    if A > B:
+        raise Exception('B must be higher than A!')
+    if K > A and K > B:
+        z = 0
+    while B%K != 0:
+        B-=1
+    while A%K != 0:
+        A+=1
+    z = ((B/K) - (A/K)) + 1
+    
+    return int(z)
